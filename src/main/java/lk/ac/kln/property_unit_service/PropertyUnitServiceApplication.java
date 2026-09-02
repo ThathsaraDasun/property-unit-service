30c8df1aab164cd000477b5c3a597d11e4f787a7
ThathsaraDasun <dasunh2004@gmail.com>
2026-09-03 00:36:27 +0530

    Initial commit: Setup property-unit-service.

---
+++ src/main/java/lk/ac/kln/property_unit_service/PropertyUnitServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.kln.property_unit_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyUnitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyUnitServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ac/kln/property_unit_service/PropertyUnitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.kln.property_unit_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyUnitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
